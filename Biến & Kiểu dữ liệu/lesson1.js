c8251c9e5ec12988174c9d772d22be94362d893b
Lê Văn Đức <leduc250824@gmail.com>
2026-03-11 21:08:13 +0700

    Buổi đầu

---
+++ Biến & Kiểu dữ liệu/lesson1.js
@@ -0,0 +1,63 @@
+// Bài 1:
+
+let name1 = "Đức";
+let age = 23;
+let height = 1.61;
+let isStudent = true;
+
+console.log(name1, age, height, isStudent);
+
+// Bài 2
+let a = 10;
+let b = 5;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
+//  Bài 3
+let lastName = "Đức";
+let fristName = "Lê";
+let fullName = fristName + " " + lastName;
+
+console.log(`Họ và tên: ${fullName}`);
+
+// Bài 4
+
+let string = "string";
+let number = 10;
+let isBoolean = true;
+
+let array = ["HTML", "CSS", "JS"];
+let object = { name: "Đức", age: 23 };
+
+console.log(string);
+console.log(number);
+console.log(isBoolean);
+console.log(array);
+console.log(object);
+
+// Bài 5
+let price = 100
+let quatity = 3
+
+let total = price * quatity
+
+console.log(total)
+
+// Bài 6
+let value = "123";
+console.log(value)
+Number.parseInt(value)
+console.log(value)
+
+// Bài 7
+
+let yearOfBirth = 2004;
+console.log(`Tuổi: ${2026 - yearOfBirth}`)
+
+
+
+
+
